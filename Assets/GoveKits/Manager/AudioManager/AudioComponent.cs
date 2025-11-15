95ff59e20309f58f814e2dbfdaa4b34051ae56e8
Gove <3425519400@qq.com>
2025-11-15 13:51:49 +0800

    Add AudioComponent and AudioConfig classes for audio management

---
+++ Assets/GoveKits/Manager/AudioManager/AudioComponent.cs
@@ -0,0 +1,57 @@
+
+
+
+
+using UnityEngine;
+
+namespace GoveKits.Manager
+{
+    // 音频组件
+    public class AudioComponent : MonoBehaviour
+    {
+        public AudioConfig[] audioConfigs;
+
+        /// <summary>
+        /// 播放音频
+        /// </summary>
+        /// <param name="soundName"></param>
+        public void Play(string soundName)
+        {
+            foreach (var config in audioConfigs)
+            {
+                if (config.configName == soundName && config.audioClip != null)
+                {
+                    if (config.isBGMLoop)  // 播放背景音乐
+                    {
+                        AudioManager.Instance.PlayBGM(config.audioClip);
+                    }
+                    else  // 播放音效
+                    {
+                        AudioManager.Instance.PlaySFX(config.audioClip, config.volume);
+                    }
+                }
+            }
+        }
+
+
+        public void Play(AudioClip clip, float volume = 1f)
+        {
+            AudioManager.Instance.PlaySFX(clip, volume);
+        }
+    }
+
+
+
+
+
+
+    // 音频配置
+    [System.Serializable]
+    public class AudioConfig
+    {
+        public string configName = "";
+        public AudioClip audioClip = null;
+        public float volume = 1f;
+        public bool isBGMLoop = false;
+    }
+}
